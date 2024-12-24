beece911904fc78a0a6e76e0ab62e8a0a253502b
Anand Thakur <anandsingh23112005@gmail.com>
2024-12-24 13:42:42 +0530

    home page complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import { Route, Routes } from "react-router"
+import Home from "./Pages/Home"
+import UserLogin from "./Pages/UserLogin"
+import UserSignUp from "./Pages/UserSignUp"
+import CaptainLogin from "./Pages/CaptainLogin"
+import CpatainSignUp from "./Pages/CpatainSignUp"
 
 
 
@@ -5,8 +11,13 @@
 function App() {
   return (
     <>
-      <h1> </h1>
-       
+     <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<UserLogin/>}/>
+        <Route path="/Signup" element={<UserSignUp/>}/>
+        <Route path="/captain-login" element={<CaptainLogin/>}/>
+        <Route path="/captain-signup" element={<CpatainSignUp/>}/>
+      </Routes>  
     </>
   )
 }
---
+++ src/Pages/CaptainLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CaptainLogin = () => {
+  return (
+    <div>
+      clog
+    </div>
+  )
+}
+
+export default CaptainLogin
