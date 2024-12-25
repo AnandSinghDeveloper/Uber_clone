70fb82dc26f4e02fe6c02ec65d0d0444591ac6f8
Anand Thakur <anandsingh23112005@gmail.com>
2024-12-25 12:02:08 +0530

    user login and captain login complete

--- src/Pages/CaptainLogin.jsx
+++ src/Pages/CaptainLogin.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CaptainLogin = () => {
   return (
+    <div className=' p-7 h-screen flex justify-between flex-col '>
     <div>
-      clog
+    <img className=' w-20 mb-10' src="http://pngimg.com/uploads/uber/uber_PNG24.png" alt="" />
+     <form className=''>
+     <h3 className=' text-xl mb-2 font-medium'>What's Your Email</h3>
+     <input required className=' bg-[#eeeeee] w-full mb-7 px-4 py-2 rounded text-lg border placeholder:text-base ' placeholder='email@example.com' type="email" />
+     <h3 className=' text-xl mb-2 font-medium'>Password</h3>
+     <input required className=' bg-[#eeeeee] w-full mb-7 px-4 py-2 rounded text-lg border placeholder:text-base ' placeholder='password' type="email" />
+     <button className='bg-black text-white w-full mb-7 px-4 py-2 rounded text-lg font-semibold '>Login</button>
+      
+     <p>Now here? <Link to='/captain-signup' className=' text-blue-500 text-base text-center'>Resister now </Link></p>
+     </form>
     </div>
+
+    <div>
+       <Link to='/login'className='bg-[#EB8B37] inline-block text-center text-[#eeeeee] w-full mb-7 px-4 py-2 rounded text-lg font-semibold '>SignIn as a User</Link>
+       </div>
+
+  </div>
   )
 }
 
--- src/Pages/UserLogin.jsx
+++ src/Pages/UserLogin.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import { Link } from 'react-router'
 const UserLogin = () => {
+   
+
   return (
-    <div>
-      ul
+    <div className=' p-7 h-screen flex justify-between flex-col '>
+      <div>
+      <img className=' w-20 mb-10' src="https://freelogopng.com/images/all_img/1659761100uber-logo-png.png" alt="" />
+       <form className=''>
+       <h3 className=' text-xl mb-2 font-medium'>What's Your Email</h3>
+       <input required className=' bg-[#eeeeee] w-full mb-7 px-4 py-2 rounded text-lg border placeholder:text-base ' placeholder='email@example.com' type="email" />
+       <h3 className=' text-xl mb-2 font-medium'>Password</h3>
+       <input required className=' bg-[#eeeeee] w-full mb-7 px-4 py-2 rounded text-lg border placeholder:text-base ' placeholder='password' type="email" />
+       <button className='bg-black text-white w-full mb-7 px-4 py-2 rounded text-lg font-semibold '>Login</button>
+        
+       <p>New here? <Link to='/Signup' className=' text-blue-500 text-base text-center'> Create New Account</Link></p>
+       </form>
+      </div>
+
+      <div>
+         <Link to='/captain-login'className='bg-[#10b461] inline-block text-center text-[#eeeeee] w-full mb-7 px-4 py-2 rounded text-lg font-semibold '>SignIn as a Captain</Link>
+         </div>
+
     </div>
   )
 }
