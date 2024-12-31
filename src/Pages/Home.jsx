19092150e378e3a9c5b8136254bd811cc2f8f8d2
Anand Thakur <anandsingh23112005@gmail.com>
2024-12-31 12:14:20 +0530

    homescreen ui created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
     <div>
       <div className=' bg-cover bg-center bg-[url(https://cdn.pixabay.com/photo/2015/09/13/11/42/green-traffic-light-938031_1280.jpg)] h-screen pt-8 flex justify-between flex-col w-full bg-green-300'>
         <img className=' w-20 ml-9' src="https://freelogopng.com/images/all_img/1659761100uber-logo-png.png" alt="" />
-        <div className=' bg-white px-5 py-8 rounded-tl rounded-tr opacity-90 '>
-          <h2 className=' text-[7vw] font-bold mt-[1vh]  leading-none tracking-tight '>Get Started with Uber</h2>
-          <Link to='/login' className=' inline-block text-center  py-[2vw] bg-black text-slate-100  w-full rounded text-[5vw] mt-4 cursor-pointer '>Continue
+        <div className=' bg-white px-[5vw] py-[8vw] rounded-tl rounded-tr opacity-90 '>
+          <h2 className=' text-2xl font-bold mt-[1vh]  leading-none tracking-tight '>Get Started with Uber</h2>
+          <Link to='/login' className=' inline-block text-center  py-[2vw] bg-black text-slate-100  w-full rounded text-xl mt-4 cursor-pointer '>Continue
 
          
           </Link>
--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { FaCaretUp } from "react-icons/fa6";
 
 const HomeScreen = () => {
   return (
-    <div>
-      HomeScreen
+    <div className=' h-screen relative'>
+        
+          <img className=' w-[20vw] absolute left-[5vw] top-[5vw]' src="https://freelogopng.com/images/all_img/1659761100uber-logo-png.png" alt="" />
+
+          <div className=' w-screen h-screen'>
+               <img className=' w-full h-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="Map image" />
+          </div>
+
+          <div className=' w-full absolute flex flex-col justify-end  top-0 rounded-tr-xl h-screen rounded-tl-xl'>
+       
+          
+            <div className=' h-[30%] p-[5vw] bg-white relative'>
+            <div className=' flex justify-center  rotate-0'>  <  FaCaretUp/> </div>
+            <h4 className=' text-2xl font-semibold'> Find Your Trip</h4>
+            <div className="line h-12 w-1 rounded top-[45%] bg-gray-800 absolute left-10 "></div>
+             <form >
+                 <input className=' w-full bg-[#eeeeee] px-8 py-3 text-base rounded mt-[5vw]' type="text" placeholder=' Add your pick-up location' />
+                 <input className='w-full bg-[#eeeeee] px-8 py-3 text-base rounded mt-[3vw]' type="text" placeholder=' Enter your destination ' />
+             </form>
+            </div>
+            <div className=' h-[70%] bg-slate-700 hidden'>
+
+            </div>
+          </div>
     </div>
   )
 }
