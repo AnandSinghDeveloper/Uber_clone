793c11b701201083f4f6e25b592b88e490f8ee10
Anand Thakur <anandsingh23112005@gmail.com>
2025-02-07 17:08:25 +0530

    somer change

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require("./index.js");
-const { initializeSocket } = require('./socket');
+// const { initializeSocket } = require('./socket');
 
 
 const port = process.env.PORT || 3000
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 
 const Server = http.createServer(app);
 
-initializeSocket(Server);
+// initializeSocket(Server);
 
 Server.listen(port,()=>{
     console.log(`Server runnig on port : ${port}`)
